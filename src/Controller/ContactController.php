13f0bbafbca4c7d94796fed3f18516b277d8e549
Michael Yapi <michaelakichi@gmail.com>
2021-06-09 21:42:54 +0000

    homepage et contact submit

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,66 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ],
+            ])
+            ->add('objet', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ],
+            ])
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            dd( $data );
+        }
+
         return $this->render('front-end/contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
